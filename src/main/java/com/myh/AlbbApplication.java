e3f0676d5244b69e6ad7d6baea8e851bd84aed3a
xiaobai-myh123 <xiaobaix25@gmail.com>
2020-04-30 12:54:29 +0800

    初始化

---
+++ src/main/java/com/myh/AlbbApplication.java
@@ -0,0 +1,13 @@
+package com.myh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbbApplication.class, args);
+	}
+
+}
